070697d44bf9af80ca79fd67fa9653f5ed3cc4f0
HRNGR <gurharun@gmail.com>
2022-04-12 01:29:43 +0300

    update

---
+++ hands-on/Terraform/terraform-04_Modules_Import/Afterclass/learn-terraform-import/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "tf-instance" {
+  ami = ""
+}
