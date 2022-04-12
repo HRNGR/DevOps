56c2cd21599dc1b29d8b11b972bfdbd05f98a1dd
HRNGR <gurharun@gmail.com>
2022-04-13 00:48:31 +0300

    project Roman Numerals Converter

--- projects/Terraform-in_class_work/provider.tf
+++ projects/Terraform-in_class_work/provider.tf
@@ -1,34 +1,11 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
-
-data "aws_ami" "tf_ami" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-}
-
 terraform {
-  requiried_providers {
-      source  = "hasicorp/aws"
-      version = "4.8.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.9.0"
+    }
   }
 }
-
-
-resource "aws_instance" "tf-ec2" {
-  ami             = data.aws_ami.tf_ami.id
-  count           = var.num_of_instance
-  instance_type   = var.ec2_type
-  key_name        = var.keyname
-  security_groups = ["tf-intance-sg"]
-  user_data = file("./scrip.sh")
--- projects/Terraform-in_class_work/variables.tf
+++ projects/Terraform-in_class_work/variables.tf
@@ -1,15 +1,9 @@
 variable "vpc_id" {
-  default = "vpc-0036a63abf476f6a5"
-  }
-
-variable "keyname" {
-    default = "FirstKey"
+  default = "vpc-0d201a8260f522c8e"
 }
-variable "awsprops" {
-  type = maps(string)
-  default = {
-      "vpc_id" = "vpc-0036a63abf476f6a5"
-      publicip = True
-      subnet = ""
-  }
+variable "keyname" {
+  default = "FirstKey"
+}
+variable "instance_type" {
+  default = "t2.micro"
 }
