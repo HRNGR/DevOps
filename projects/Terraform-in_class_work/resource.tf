03d0347725996aa38c795e607cffbdccb0ac9de1
HRNGR <gurharun@gmail.com>
2022-04-12 22:34:55 +0300

    update

--- projects/Terraform-in_class_work/resource.tf
+++ projects/Terraform-in_class_work/resource.tf
@@ -1,5 +1,31 @@
 resource "aws_security_group" "webserverSG" {
   name = "Web Server Sec Group"
   description = "Accepts HTTP and SSH Traffic"
-  vpc = ""
+  vpc = var.vpc_id #lookup(var.awsprops, "vpc_id")
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
 }
