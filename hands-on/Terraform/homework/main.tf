9af7553f1d92d1bd4274a1b51909c102cfb6667c
HRNGR <gurharun@gmail.com>
2022-04-12 18:21:04 +0300

    homework

--- hands-on/Terraform/homework/main.tf
+++ hands-on/Terraform/homework/main.tf
@@ -7,3 +7,42 @@ Terraform {
     }
 }
 
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_security_group" "name" {
+  name = "odev-sg"
+    tags = {
+    Name = "tf-sg"
+  }
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
+
+
+data "aws_ami" "my_ami" {
+    most_recent = true
+  
+}
