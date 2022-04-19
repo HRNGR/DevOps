4db7809db96b03eac90441b335d1541d9ba2eda7
HRNGR <gurharun@gmail.com>
2022-04-19 13:43:22 +0300

    Update

--- hands-on/Docker/docker-05-image-basic-operations/welcome.py
+++ hands-on/Docker/docker-05-image-basic-operations/welcome.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "<h1>Welcome to Clarusway</h1>"
+    return "<h1>Welcome to Harun's Website</h1>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
