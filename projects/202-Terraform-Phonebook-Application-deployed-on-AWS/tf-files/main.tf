951165c5edfcb2bf04f99e47ef6564f8ecc0b230
HRNGR <gurharun@gmail.com>
2022-05-03 23:46:56 +0300

    repo

--- projects/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/main.tf
+++ projects/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/main.tf
@@ -105,7 +105,7 @@ resource "aws_db_instance" "db-server" {
 resource "github_repository_file" "dbendpoint" {
   content = aws_db_instance.db-server.address
   file = "dbserver.endpoint"
-  repository = "phonebooktf" #repo adı yazılacak önemli
+  repository = "phonebooktf" # repo adı yazılacak önemli
   overwrite_on_create = true
   branch = "master"
 }
