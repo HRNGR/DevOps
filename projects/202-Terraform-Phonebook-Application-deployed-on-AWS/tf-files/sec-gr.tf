40d7c435d122e4c4f4dc8c0d43b76f75b480e8ef
HRNGR <gurharun@gmail.com>
2022-05-03 23:44:40 +0300

    update

--- projects/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/sec-gr.tf
+++ projects/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/sec-gr.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "db-sg" {
   }
 
   ingress {
-    from_port = 3306 #rds için
+    from_port = 3306 # rds için
     protocol = "tcp"
     to_port = 3306
     security_groups = [aws_security_group.server-sg.id]
