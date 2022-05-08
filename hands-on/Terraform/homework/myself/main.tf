f9bdd43e63070fad809c6a06be8c6545692845ae
HRNGR <gurharun@gmail.com>
2022-05-08 23:09:01 +0300

    AMI ID

--- hands-on/Terraform/homework/myself/main.tf
+++ hands-on/Terraform/homework/myself/main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "my_ami" {
 }
 
 resource "aws_instance" "hw-ec2" {
-  ami = ""
+  ami = ""  ## AMI ID yazılacak
   instance_type = "t2.micro"
   key_name = "FirstKey"
   security_groups = ["hw-sg"]
