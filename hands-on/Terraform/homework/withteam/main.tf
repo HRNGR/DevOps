2bb6f3acabfe3a903ad0f64b2f25fd1d2ab141e2
HRNGR <gurharun@gmail.com>
2022-05-08 23:12:03 +0300

    SOR

--- hands-on/Terraform/homework/withteam/main.tf
+++ hands-on/Terraform/homework/withteam/main.tf
@@ -87,12 +87,14 @@ resource "aws_security_group" "tf-sg" {
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
   ingress {
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     protocol    = -1
