1db87d0d0ee99b7c8385592c75516e97fa2a05c2
HRNGR <gurharun@gmail.com>
2022-05-23 19:04:27 +0300

    version

--- hands-on/Maven/maven-01-using-maven-as-a-build-tool/maven-java-terraform-template.tf
+++ hands-on/Maven/maven-01-using-maven-as-a-build-tool/maven-java-terraform-template.tf
@@ -10,7 +10,7 @@ provider "aws" {
   //  If you have entered your credentials in AWS CLI before, you do not need to use these arguments.
 }
 
-resource "aws_instance" "maven-ec2" {
+resource "aws_instance" "maven-ec21" {
   ami             = "ami-0947d2ba12ee1ff75"
   instance_type   = "t2.micro"
   
